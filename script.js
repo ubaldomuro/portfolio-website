2631f552d5d0a0d7e9393c690666f84d5fba201a
ubaldo <ubaldo.muro@gmail.com>
2022-02-11 15:12:34 -0600

    halfway there js game

--- script.js
+++ script.js
@@ -1,42 +1,76 @@
 let canvas = document.getElementById("myCanvas");
 let ctx = canvas.getContext("2d");
 
+//ball variables
 let x = canvas.width/2;
 let y = canvas.height-30;
 let dx = 2;
 let dy = -2;
+let ballRadius = 10;
+
+//paddle variables
+let paddleHeight = 10;
+let paddleWidth = 75;
+let paddleX = (canvas.width-paddleWidth)/2;
+let rightPressed = false;
+let leftPressed = false;
+
 
 let draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawPaddle();
     drawBall();
     x += dx;
     y += dy;
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) { //to make the ball bounce off the canvas borders
+        dx = -dx;
+    }
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) { //to make the ball bounce off the canvas borders
+        dy = -dy;
+    }
+    if(rightPressed && paddleX < canvas.width-paddleWidth) {
+        paddleX += 7;
+    }
+    else if(leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }Ñ
 };
 
 let drawBall = () => {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 };
 
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function keyDownHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.keyCode == 39) {
+        rightPressed = false;
+    }
+    else if(e.keyCode == 37) {
+        leftPressed = false;
+    }
+}
+
 setInterval(draw, 10);
-
-// ctx.beginPath();
-//     ctx.rect(20, 40, 50, 50);
-//     ctx.fillStyle = "#FF0000";
-//     ctx.fill();
-// ctx.closePath();
-
-// ctx.beginPath();
-//     ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-//     ctx.fillStyle = "green";
-//     ctx.fill();
-// ctx.closePath();
-
-// ctx.beginPath();
-//     ctx.rect(160, 10, 100, 40);
-//     ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-//     ctx.stroke();
-// ctx.closePath();
